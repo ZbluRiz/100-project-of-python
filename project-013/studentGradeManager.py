da56325121e1deb1336e3c1a39d37827ebf0f81f
ZbluRiz <rizkybayu1254@gmail.com>
2026-02-16 18:41:32 +0700

    student grade manager

--- project-013/studentGradeManager.py
+++ project-013/studentGradeManager.py
@@ -1,6 +1,25 @@
+value = 0
 #enter the student scores
 student_scores = input("enter stude scores separated by commas: ")
 scores = [int(score) for score in student_scores.split(',')]
+
+for i in scores:
+    value += i
+    
+averages = value / len(scores)
+
+while True:
+    choice = input("type (descending/ascending) for sorting: ")
+    
+    if choice == 'ascending':
+        scores = sorted(scores)
+        break
+    elif choice == 'descending':
+        scores = sorted(scores,reverse=True)
+        break
+    else:
+        print("input the right choice")
+        break
 
 #filter score by grades
 grades = [
@@ -16,10 +35,14 @@ grades = [
 passing_students= [score for score in scores if score >= 60]
 failing_students = [score for score in scores if score < 60]
 
+
 print("\n-Students Grade-")
 for i,(score,grade) in enumerate(zip(scores,grades),start=1):
     print(f"Student {i} : Score = {score}, Grade = {grade}")
-
+    
 print("\n-Passing or Failling Students-")
+print(f"Highest Scores: {max(scores)}")
+print(f"Lowest Scores: {min(scores)}")
+print(f"Average Student Score: {averages:.2f}")
 print(f"Passing Students: {passing_students}")
 print(f"Failing Students: {failing_students}")
